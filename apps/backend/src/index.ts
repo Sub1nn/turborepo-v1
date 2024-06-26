79928daf1d65ba6918c46b9efbcdc597838efae4
Subin Khatiwada <Subinkhatiwada@gmail.com>
2024-06-26 20:37:24 +0300

    imported backend url from common module

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,10 +2,13 @@ import express from "express";
 
 const app = express();
 
+import { BACKEND_URL } from "@repo/common/config";
+
+console.log(BACKEND_URL);
 app.get("/", (req: any, res: any) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("App listening at http://localhost:3000");
+app.listen(3005, () => {
+  console.log("App listening at http://localhost:3005");
 });
